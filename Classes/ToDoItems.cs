2c937a352252e41efb337481549ebf1843bc2554
Tobe-Deleted <oso082024@gmail.com>
2025-03-19 11:15:58 +0100

    Init commit

---
+++ Classes/ToDoItems.cs
@@ -0,0 +1,9 @@
+namespace ToDoAPI.Models
+{
+    public class ToDoItems
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
